a5c74d7300ff91dd692d6e7c6808b26564a73b12
Fernandes Pinheiro <msfp@gft.com>
2024-09-14 15:19:42 -0300

    fix: mongo connect url

--- src/infrastructure/database/config/mongoose.ts
+++ src/infrastructure/database/config/mongoose.ts
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://v4:v4123@v4.csijo.mongodb.net/?retryWrites=true&w=majority&appName=v4', {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://v4:v4123@v4.csijo.mongodb.net/?retryWrites=true&w=majority&appName=v4');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
